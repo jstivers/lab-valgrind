ca3e52fdfa0212715c146826bb642e118014b7c2
John Stivers <jstivers@usc.edu>
2017-02-28 16:43:16 -0800

    push

--- scramble.cpp
+++ scramble.cpp
@@ -6,25 +6,62 @@
 #include <cstdlib>
 #include <ctime>
 #include <cstring>
-
+#include <fstream>
 using namespace std;
 
 // Scramble the letters of this string randomly
 void permute(char items[], int len);
 
 // Define an array of strings (an array of char pointers, like argv)
-const char* wordBank[] = {
-   "computer", "president", "trojan", "program", "coffee",
-   "library", "football", "popcorn", "science", "engineer"};
 
-const int numWords = 10;
 
-int main() {
+
+int main(int argc, char *argv[] ) {
   srand(time(0));
   char guess[80];
 
-  bool wordGuessed = false;
-  int numTurns = 10;
+
+    bool wordGuessed = false;
+    int numTurns = 10;
+    int numWords;
+    char buffer[41];
+
+
+
+    //checking command line arguments
+    if ( argc != 1){
+        return 0;
+    }
+
+
+    //opening the fle
+    fstream  file;
+    file.open(argv[1], ios::out | ios::in );
+    if ( !file.is_open() ){
+        cout<<"\nFILE WAS NOT SUCCESSFULLY OPENED BY SCRAMBLE.CPP, PROGRAM SHUTTING DOWN\n";
+        return 0;
+    }
+
+
+  // reading from the file
+    std::fstream input(argv[1]);
+    std::string str;
+    string line;
+
+    if(!(file >> numWords)){
+        cout<<"\nNUMBER OF WORDS WAS NOT DETECTED BY SCRAMBLE.CPP, PROGRAM SHUTTING DOWN\n";
+        return 0;
+    }else{
+        file >> numWords;
+    }
+    getline( file, line );
+
+    // Now begin your useful code
+    while( file.eof() ) {
+        // This will just over write the first line read
+        getline( file, line );
+
+    }
 
   // Pick a random word from the wordBank
   int target = rand() % numWords;
